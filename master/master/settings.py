87785a32f14f88e6d2403a1cf63020fab57fbec9
Jakub Gielniak <jbgielniak@gmail.com>
2025-01-06 13:06:37 +0100

    Got working assignment

--- master/master/settings.py
+++ master/master/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'storage.middleware.CurrentUserMiddleware',
 ]
 
 ROOT_URLCONF = 'master.urls'
---
+++ master/storage/signals.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from . import models
+from .middleware import get_current_user
+
+@receiver(post_save, sender=models.ItemPlaced)
+def create_link_for_new_placement(sender, instance, created, **kwargs):
+    if created:
+        user = get_current_user()
+        models.UserPlacement.objects.create(placement=instance, quantity=instance.no_in_packs*instance.quantity_of_packs, user=user)
